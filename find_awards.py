d8f8168b70703436042ce80f7d1b1c4b376529b7
latif93 <108307359+latif93@users.noreply.github.com>
2023-01-23 16:12:00 -0600

    took out hosts to separate into other file

--- find_awards.py
+++ find_awards.py
@@ -30,17 +30,3 @@ for award in awards_freq_dict_sorted_by_highest_freq[0:50]:
         final_awards_lst.append(award)    
 print(final_awards_lst)
 #There are somehwere between 25-30 awards and we have a lot of false positives right now
-#------------------------Experimention with hosts--------------------------------------
-host_freq_dict  = dict()
-
-for tweet in tweets:
-    if "host" in tweet["text"]:
-        hosts = re.findall("[A-Z][a-z]* [A-Z][a-z]*", tweet["text"]) 
-        #find all strings where two capitalized words are together in tweets with the word "host" in them
-        for host in hosts:
-        	add_to_dict(host, host_freq_dict) #filling host frequency dictionary
-host_freq_dict_sorted_by_highest_freq = sorted(host_freq_dict, key = host_freq_dict.get)[::-1]
-print(host_freq_dict_sorted_by_highest_freq[0:5]) 
-#Will Ferrel is first for some reason, although second and third place where the real hosts. Gotta think about optimization here.
-
-
