0f59a416231ac5c85d54fc0698e42f004135510d
latif93 <108307359+latif93@users.noreply.github.com>
2023-01-25 16:41:29 -0600

    find_nominees now ports imdb data; daniyal

--- find_nominees.py
+++ find_nominees.py
@@ -1,15 +1,21 @@
 import re
 import json 
+import requests
+import ast
+from  data import get_2013_award_data
+
 
 file = open('gg2013.json')
 tweets = json.load(file)
 file.close()
 
+
+print(get_2013_award_data())
 def add_to_freq_dict(item, freq_dict):
     if item not in freq_dict:
         freq_dict[item] = 1
     else:
-        freq_dict[item] +=1
+        freq_dict[item] += 1
 
 awards = ["Best Performance by an Actor in a Motion Picture", "Best Supporting Actress", "Best Motion Picture"] #hardcoding some awards to make a rough draft using
 
@@ -17,7 +23,6 @@ award_to_nominee_dict = dict()
 
 for award in awards:
     award_to_nominee_dict[award] = dict()
-#print(award_to_nominee_dict)
 for tweet in tweets:
     for award in awards:
         if award in tweet["text"]:
@@ -28,4 +33,4 @@ for tweet in tweets:
 for award in award_to_nominee_dict:
     award_to_nominee_dict[award] = sorted(award_to_nominee_dict[award], key = award_to_nominee_dict[award].get)[::-1]
 
-print(award_to_nominee_dict)
+#print(award_to_nominee_dict)
