30881d7677ebf4bef46c34ef51b72bfdbfe4c70d
latif93 <108307359+latif93@users.noreply.github.com>
2023-02-01 10:18:47 -0600

    made get_2015_award_data() function
    the 2015 award data is in the same format as I did for 2013 so if your code parses the 2013 data it shouldn't need to change for 2015

--- data.py
+++ data.py
@@ -10,16 +10,28 @@ golden_globes_2013_url = "https://www.imdb.com/event/ev0000292/2013/1/?ref_=gg_e
 golden_globes_2015_url = "https://www.imdb.com/event/ev0000292/2015/1/?ref_=ev_eh"
 
 golden_globes_2013_html = requests.get(golden_globes_2013_url).text
+golden_globes_2015_html = requests.get(golden_globes_2015_url).text
 
 start_index_of_golden_globes_2013_info = golden_globes_2013_html.index("IMDbReactWidgets.NomineesWidget.push") + len("IMDbReactWidgets.NomineesWidget.push") + 19
 end_index_of_golden_globes_2013_info = golden_globes_2013_html.index("true}}") + len("true}}")
+
+start_index_of_golden_globes_2015_info = golden_globes_2015_html.index("IMDbReactWidgets.NomineesWidget.push") + len("IMDbReactWidgets.NomineesWidget.push") + 19
+end_index_of_golden_globes_2015_info = golden_globes_2015_html.index("true}}") + len("true}}")
+
 golden_globes_2013_info = golden_globes_2013_html[start_index_of_golden_globes_2013_info:end_index_of_golden_globes_2013_info]
 golden_globes_2013_info = golden_globes_2013_info.replace("null", "None")
 golden_globes_2013_info = golden_globes_2013_info.replace("true", "True")
 golden_globes_2013_info = golden_globes_2013_info.replace("false", "False")
 golden_globes_2013_info_dict = ast.literal_eval(golden_globes_2013_info)
 
+golden_globes_2015_info = golden_globes_2015_html[start_index_of_golden_globes_2015_info:end_index_of_golden_globes_2015_info]
+golden_globes_2015_info = golden_globes_2015_info.replace("null", "None")
+golden_globes_2015_info = golden_globes_2015_info.replace("true", "True")
+golden_globes_2015_info = golden_globes_2015_info.replace("false", "False")
+golden_globes_2015_info_dict = ast.literal_eval(golden_globes_2015_info)
+
 golden_globe_2013_data_str = ""
+golden_globe_2015_data_str = ""
 
 for i, category in enumerate(golden_globes_2013_info_dict["nomineesWidgetModel"]["eventEditionSummary"]["awards"][0]["categories"]):
     golden_globe_2013_data_str += category['categoryName']
@@ -27,6 +39,16 @@ for i, category in enumerate(golden_globes_2013_info_dict["nomineesWidgetModel"]
         golden_globe_2013_data_str += "\n"
         golden_globe_2013_data_str += nomination["primaryNominees"][0]["name"] 
     golden_globe_2013_data_str += "\n\n"
-    
+
+for i, category in enumerate(golden_globes_2015_info_dict["nomineesWidgetModel"]["eventEditionSummary"]["awards"][0]["categories"]):
+    golden_globe_2015_data_str += category['categoryName']
+    for nomination in golden_globes_2015_info_dict["nomineesWidgetModel"]["eventEditionSummary"]["awards"][0]["categories"][i]["nominations"]:
+        golden_globe_2015_data_str += "\n"
+        golden_globe_2015_data_str += nomination["primaryNominees"][0]["name"] 
+    golden_globe_2015_data_str += "\n\n"
+
 def get_2013_award_data():
 	return golden_globe_2013_data_str
+
+def get_2015_award_data():
+	return golden_globe_2015_data_str
