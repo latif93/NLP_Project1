50f539f0ba8e43c0c4b83be0a30e8c25dca00698
latif93 <108307359+latif93@users.noreply.github.com>
2023-02-04 16:19:47 -0600

    works with autograder
    awards we don't have much data for aren't working well right now. everything else works well

--- find_nominees.py
+++ find_nominees.py
@@ -2,43 +2,63 @@ import re
 import json 
 import requests
 import ast
-from  data import get_2013_award_data
-from  data import get_2015_award_data
+from data import get_2013_award_data
+from data import get_2015_award_data
 
+def import_json_file(year):
+    filename = f"gg{year}.json"
+    try:
+        file = open(filename)
+        tweets = json.load(file)
+        file.close()
+        return tweets
+    except FileNotFoundError:
+        print("Could not find the file " + filename)
+        sys.exit()
 
-file = open('gg2013.json')
-tweets = json.load(file)
-file.close()
-
-award_data_2013 = get_2013_award_data()
-print(award_data_2013)
 def add_to_freq_dict(item, freq_dict):
     if item not in freq_dict:
         freq_dict[item] = 1
     else:
         freq_dict[item] += 1
-
-awards = ["Best Performance by an Actress in a Television Series - Drama", "Best Performance by an Actress in a Miniseries or Motion Picture Made for Television", "Best Animated Feature Film"] #hardcoding some awards to make a rough draft using
-
-award_to_nominee_dict = dict()
-nominees_dict = dict()
-award_words = dict()
-for award in awards:
-    award_to_nominee_dict[award] = dict()
-    nominees_dict[award] = award_data_2013.split(f"{award}\n")[1].split("\n\n")[0].split("\n")
-    award_words[award] = award.split(" ") 
-print(award_words)
-for tweet in tweets:
+def create_lists_of_potential_nominees(year, tweets, awards):
+    award_data = ""
+    if year == "2013":
+        award_data = get_2013_award_data()
+    if year == "2015":
+        award_data = get_2015_award_data()
+    award_to_nominee_dict = dict()
+    nominees_dict = dict()
+    award_words = dict()
     for award in awards:
-    	for word in award_words[award]:
-	        if word in tweet["text"]:
-	            nominee_names = re.findall("[A-Z][a-z]*[- ]?[A-Z][a-z]*", tweet["text"])
+        award_to_nominee_dict[award] = dict()
+        if award in award_data.lower():
+            nominees_dict[award] = award_data.lower().split(f"{award.lower()}\n")[1].split("\n\n")[0].split("\n")
+        award_words[award] = award.split(" ") 
+    count = 0
+    for tweet in tweets:
+        #print(count)
+        count += 1
+        for award in awards:
+            for word in award_words[award]:
+                if word in tweet["text"].lower() and word != "-":
+                    nominee_names = re.findall("[A-Z][a-z]*[- ]?[A-Z][a-z]*", tweet["text"])
+                    for nominee_name in nominee_names:
+                #        if award not in nominees_dict:
+                #            if len(award_to_nominee_dict[award]) < 7:
+                #                add_to_freq_dict(nominee_name, award_to_nominee_dict[award])
+                #        else:
+                        if award not in nominees_dict:
+                            nominees_dict[award] = []
+                            add_to_freq_dict(nominee_name, award_to_nominee_dict[award])
+                        if nominee_name.lower() in nominees_dict[award]:
+                            add_to_freq_dict(nominee_name, award_to_nominee_dict[award])
+    for award in award_to_nominee_dict:
+        award_to_nominee_dict[award] = sorted(award_to_nominee_dict[award], key = award_to_nominee_dict[award].get)[::-1]
+    return award_to_nominee_dict
 
-	            for nominee_name in nominee_names:
-	            	if nominee_name in nominees_dict[award]:
-	                    add_to_freq_dict(nominee_name, award_to_nominee_dict[award])
+def find_nominees(year, awards):
+    tweets = import_json_file(year)
+    award_nominees = create_lists_of_potential_nominees(year, tweets, awards)
+    return award_nominees
 
-for award in award_to_nominee_dict:
-    award_to_nominee_dict[award] = sorted(award_to_nominee_dict[award], key = award_to_nominee_dict[award].get)[::-1]
-
-print(award_to_nominee_dict)
