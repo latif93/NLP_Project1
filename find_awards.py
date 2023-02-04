2958bf248de339713e53ea12b79ba599d61a4254
latif93 <108307359+latif93@users.noreply.github.com>
2023-02-04 16:14:41 -0600

    not much changed here, still need to tune accuracy

--- find_awards.py
+++ find_awards.py
@@ -1,34 +1,42 @@
 import json
 import re
 
-file = open('gg2013.json')
-tweets = json.load(file)
-file.close()
+def import_json_file(year):
+    filename = f"gg{year}.json"
+    try:
+        file = open(filename)
+        tweets = json.load(file)
+        file.close()
+        return tweets
+    except FileNotFoundError:
+        print("Could not find the file " + filename)
+        sys.exit()
 
 def add_to_freq_dict(item, freq_dict):
     if item not in freq_dict:
         freq_dict[item] = 1
     else:
         freq_dict[item] +=1
+def match_potential_awards(tweets):
+    award_matcher = "Best (.*?) [a-z]" #matches a phrase where Best is followed by capitalized words until a non-capitalized one is found
+    awards_freq_dict = dict()
 
-award_matcher = "Best (.*?) [a-z]" #matches a phrase where Best is followed by capitalized words until a non-capitalized one is found
-awards_freq_dict = dict()
+    for i, tweet in enumerate(tweets):
+        match_obj = re.search(award_matcher, tweet["text"])
+        if "best" in tweet["text"].lower() and match_obj:
+            add_to_freq_dict(match_obj.group(), awards_freq_dict) #filling award frequency dictionary
+    awards_freq_dict_sorted_by_highest_freq = sorted(awards_freq_dict, key = awards_freq_dict.get)[::-1]
 
-for i, tweet in enumerate(tweets):
-    match_obj = re.search(award_matcher, tweet["text"])
-    if "best" in tweet["text"].lower() and match_obj:
-        add_to_freq_dict(match_obj.group(), awards_freq_dict) #filling award frequency dictionary
-awards_freq_dict_sorted_by_highest_freq = sorted(awards_freq_dict, key = awards_freq_dict.get)[::-1]
-
-final_awards_lst = []
-duplicate_lst = []
-for award in awards_freq_dict_sorted_by_highest_freq[0:50]: 
-    if award[-2] == " ":
-        award = award[0:-2] #fixing formatting of strings
-    if award.upper() not in duplicate_lst: #weeding out duplicates with different cases (i.e "Best picture and Best Picture")
-        duplicate_lst.append(award.upper())
-        final_awards_lst.append(award)    
-print(final_awards_lst)
-#There are somehwere between 25-30 awards and we have a lot of false positives right now
-
-
+    final_awards_lst = []
+    duplicate_lst = []
+    for award in awards_freq_dict_sorted_by_highest_freq[0:50]: 
+        if award[-2] == " ":
+            award = award[0:-2] #fixing formatting of strings
+        if award.upper() not in duplicate_lst: #weeding out duplicates with different cases (i.e "Best picture and Best Picture")
+            duplicate_lst.append(award.upper())
+            final_awards_lst.append(award) 
+    return final_awards_lst  
+def find_awards(year):
+    tweets = import_json_file(year)
+    print(match_potential_awards(tweets))
+    return match_potential_awards(tweets)
